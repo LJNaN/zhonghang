942f01be93eb7ba31c69c19112ea34f8b82356fa
柳江南 <oncwnuA5PbQxtTysj5w_BTxJ_rRU@git.weixin.qq.com>
2023-10-18 23:41:02 +0800

    54646

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index.js"
-import 'animate.css';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
